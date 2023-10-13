274c467de48819de4d9f782635bb01795bfe94ed
YoussefMagedMohamed <joeyoussef307@gmail.com>
2023-10-13 19:50:20 +0300

    Add Addresses To User

--- src/Modules/Address/address.routes.js
+++ src/Modules/Address/address.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import * as addresses from "./address.controller.js";
+import { allowedTo, protectedRoutes } from "../Auth/auth.controller.js";
 
 const addressRouter = express.Router();
 
 addressRouter
   .route("/")
-  .patch(addresses.addAddress)
-  .delete(addresses.removeAddress)
-  .get(addresses.getAllUserAddresses);
+  .patch(protectedRoutes, allowedTo("user"),addresses.addAddress)
+  .delete(protectedRoutes, allowedTo("user"),addresses.removeAddress)
+  .get(protectedRoutes, allowedTo("user"),addresses.getAllUserAddresses);
 
 export default addressRouter;
